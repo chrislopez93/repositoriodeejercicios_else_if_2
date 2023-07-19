de12f8de3bdf2342680bf7fc8d20647be215b986
chrislopez93 <sinretel@gmail.com>
2023-07-19 09:32:33 -0400

    resolución básica imc

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function action(){
     const pesovar = document.querySelector('.peso').value;
     const alturavar = document.querySelector('.altura').value;
-    const imc = (alturavar * alturavar)/ pesovar;
+    const imc = (pesovar/(alturavar * alturavar))*10000;
     pvar = document.querySelector('.p');
     pvar.innerHTML = imc;
 }
