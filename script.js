d50948340918b70995d13e7373d82c45e08ef58f
chrislopez93 <sinretel@gmail.com>
2023-07-19 23:48:47 -0400

    Creación de medidor de indice IMC

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ function action(){
     const imc = (pesovar/(alturavar * alturavar))*10000;
     pvar = document.querySelector('.p');
     pvar.innerHTML = imc;
+    if (imc < 18.5){
+        tipovar = document.querySelector('.tipo');
+        tipovar.innerHTML= "Tiene un IMC anormal";
+    }else if(imc <= 25){
+        tipovar = document.querySelector('.tipo');
+        tipovar.innerHTML= "Tiene un IMC normal";
+    } else {
+        tipovar = document.querySelector('.tipo');
+        tipovar.innerHTML= "Tiene un IMC anormal";
+    }
 }
